bb5c4a9bad6ebb6a500cc09b96b07478efdf739e
skobraf <skobraf@gmail.com>
2018-12-06 16:10:44 +0100

    added freindship callback methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_145555) do
+ActiveRecord::Schema.define(version: 2018_12_06_150520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_12_06_145555) do
   create_table "freindships", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.integer "request_sender"
+    t.integer "request_receiver"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
