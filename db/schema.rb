c0ba2f293bb90716138a63934d1983c83f6760de
skobraf <skobraf@gmail.com>
2018-12-06 16:55:47 +0100

    added comment model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_154702) do
+ActiveRecord::Schema.define(version: 2018_12_06_155135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "freindships", force: :cascade do |t|
     t.integer "sender_id"
